fa5cd5c0e0f5eb467bb198382945a1825a13bce9
Alexander Pehm <alexander@pehm.biz>
2020-01-05 23:47:57 +0100

    Added formula evaluation function

---
+++ src/utils/formula.spec.ts
@@ -0,0 +1,8 @@
+import {evaluate} from './formula';
+
+describe('formula', () => {
+
+    it('concatenates two strings to represent a path', () => {
+        expect(evaluate('foo || \'/\'  || bar', {foo: 'abc', bar: 'def'})).toEqual('abc/def');
+    });
+})
